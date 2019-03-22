7f2f216ef445877d923f53c5a8a7d74e489ef687
fakyl <fatoukine.k@icloud.com>
2019-03-22 18:15:02 +0000

    Add files via upload

--- generation/generation_svg.py
+++ generation/generation_svg.py
@@ -1,12 +1,11 @@
-#importation de modules
+# coding=utf-8
+
 import os
 
-#definition d'une fonction pour la generation en svg
+
 def generate_mcd(outputfile, dictEntity, dictAssoc):
-    
     # Recupération du nom fichier de sortie et création du fichier .mcd
     out = outputfile.replace(".svg", ".mcd")
-    
     # Ouverture du fjchier en écriture
     output = open(out, "w")
 
@@ -18,7 +17,6 @@ def generate_mcd(outputfile, dictEntity, dictAssoc):
             if idx == 1:
                 output.write(", ")
         output.write("\n")
-
     # Parcours du dictionnaire des associations et écriture dans le fichier .mcd
     for entity in dictEntity:
         output.writelines(entity + ": ")
@@ -30,5 +28,8 @@ def generate_mcd(outputfile, dictEntity, dictAssoc):
     return out
 
 # génération du fichier .svg grâce au module mocodo
-def generate_svg(file):
-    os.system("mocodo --input " + file)
+def generate_svg_json(file):
+    os.system("mocodo --input file/data_json.mcd")
+
+def generate_svg_xml(file):
+    os.system("mocodo --input file/data_xml.mcd")

717b887a455bc3c1e5b7391041272dceab711596
fakyl <fatoukine.k@icloud.com>
2019-03-22 18:36:01 +0000

    Add files via upload

--- cmd_config/cmd.py
+++ cmd_config/cmd.py
@@ -1,17 +1,14 @@
-#importation des modules
+# coding=utf-8
+
 import sys
 import getopt
 
-#deffinition d'une fonction pour la validation de commande avec en parametres les differents arguments: type de fichier avec un input et un output
-def validation_cmd(fileType, urlFluxHTTP, inputFile, outputFile):
 
-    #specification des valeurs pour le format: soite xml ou json et indication du format recherche si le type renseigne est different de ceux-la
+def validation_cmd(fileType, urlFluxHTTP, inputFile, outputFile):
     if fileType != 'xml' and fileType != 'json':
         print(
             "La commande doit être de ce format : \n \tXJ_Convertor [-i xml/json] [-t ][-h url_FluxHTTP] [-f FichierInput] -o nomFichier.svg")
         sys.exit()
-
-    #specification d'un input en flux http   
     if (urlFluxHTTP == '' and inputFile == '') or (urlFluxHTTP != '' and inputFile != ''):
         print(
             "La commande doit être de ce format : \n \tXJ_Convertor [-i xml/json] [-t ][-h url_FluxHTTP] [-f FichierInput] -o nomFichier.svg")
@@ -22,7 +19,6 @@ def validation_cmd(fileType, urlFluxHTTP, inputFile, outputFile):
         sys.exit()
     return True
 
-#definition d'une fonction indiquant si le fichier est en local ou correspond a un url
 def url_or_local(urlFluxHTTP, inputFile):
     if urlFluxHTTP == '' and inputFile != '':
         return 'local'
